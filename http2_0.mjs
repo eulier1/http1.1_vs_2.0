89299d3d4d5766418278f07c77e52f81abd1435d
Eulier González <eulier1@gmail.com>
2024-11-10 17:52:42 -0300

    add push server example

--- http2_0.mjs
+++ http2_0.mjs
@@ -20,8 +20,25 @@ http2Server.on('stream', (stream, headers) => {
       stream.respond({
         'content-type': 'text/html',
         ':status': 200
-      });
-      stream.end('<h1>Hello HTTP/2!</h1>');
+    });
+    stream.end('<html><body><h1>HTTP/2 Page</h1></body></html>');
+
+    // Server Push: Proactively send resources we know the client will need.
+    // Only if the client allows it.
+
+    // const pushStream1 = stream.pushStream({ ':path': '/image.jpg' }, (err, pushStream) => {
+    //     if (err) throw err;
+    //     pushStream.respond({ ':status': 200, 'content-type': 'image/jpeg' });
+    //     pushStream.end('Simulated image data');
+    // });
+
+    // const pushStream2 = stream.pushStream({ ':path': '/script.js' }, (err, pushStream) => {
+    //     if (err) throw err;
+    //     pushStream.respond({ ':status': 200, 'content-type': 'application/javascript' });
+    //     pushStream.end('console.log("Script loaded")');
+    // });
+
+    break;
       break;
 
     case '/api':
